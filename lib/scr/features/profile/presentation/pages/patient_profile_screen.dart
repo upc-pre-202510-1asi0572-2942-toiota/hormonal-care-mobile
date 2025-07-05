d929b5687790bb948b2ef3cc128dab24f39638ab
Claudio Sandro Quispesivana Torres <u202215099@upc.edu.pe>
2025-07-05 11:11:46 -0500

    refact: files

--- lib/scr/features/profile/presentation/pages/patient_profile_screen.dart
+++ lib/scr/features/profile/presentation/pages/patient_profile_screen.dart
@@ -137,7 +137,6 @@ class _PatientProfileScreenState extends State<PatientProfileScreen> {
                 },
               ),
             ],
-            // ...Opcional: modo edición...
           ],
         ),
       ),
